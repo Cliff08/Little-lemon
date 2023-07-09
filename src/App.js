88ae0594458eb5abd2dfb029f49f6490339a662a
Cliff08 <cliffyang_2022@qq.com>
2023-07-09 10:33:59 +0800

    the second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 function App() {
   return (
-    <div className="App">
-       Homepage
-    </div>
+    <>
+    <Header/>
+    <Nav/>
+    <Main/>
+    <Footer/>
+    </>
   );
 }
 
